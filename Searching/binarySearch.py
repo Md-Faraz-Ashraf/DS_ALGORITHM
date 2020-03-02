8fbc6d896b40774df6cec825607741a2e44f319b
mohammad.ashrafm <mohammad.ashraf@infosys.com>
2020-03-02 23:07:24 +0530

    Linear and Binary Search

---
+++ Searching/binarySearch.py
@@ -0,0 +1,22 @@
+def linearSearch(a, key):
+    for element in a:
+        if element == key:
+            return 'Found'
+    return 'Not Found'
+
+
+def binarySearch(a, key):
+    n = len(a)
+    if n == 1:
+        if a[0] == key:
+            return 'Found'
+        return 'Not Found'
+    mid = n // 2
+    if a[mid] == key:
+        return 'Found'
+    elif a[mid] <= key:
+        return binarySearch(a[mid:], key)
+    else:
+        return binarySearch(a[:mid], key)
+
+
